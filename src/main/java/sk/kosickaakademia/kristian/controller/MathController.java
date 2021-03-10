f273e77c0c40d747b1d0863c7e68cf112cba118c
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-10 11:53:51 +0100

    Added method multiply

--- src/main/java/sk/kosickaakademia/kristian/controller/MathController.java
+++ src/main/java/sk/kosickaakademia/kristian/controller/MathController.java
@@ -3,21 +3,37 @@ package sk.kosickaakademia.kristian.controller;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MathController {
     @PostMapping(path = "/add")
-    public String addTwoNumbers(@RequestBody String input){
+    public ResponseEntity<String> addTwoNumbers(@RequestBody String input){
         try {
             JSONObject object = (JSONObject) new JSONParser().parse(input);
             int a = Integer.parseInt(String.valueOf(object.get("a")));
             int b = Integer.parseInt(String.valueOf(object.get("b")));
-            System.out.println(a + b);
-        } catch (ParseException e) { e.printStackTrace(); }
+            int result = a + b;
+            JSONObject resultObject = new JSONObject();
+            resultObject.put("result", result);
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(resultObject.toJSONString());
+        } catch (ParseException e) { e.printStackTrace();
+        } catch (NumberFormatException e) {
+            JSONObject object = new JSONObject();
+            object.put("error", "Incorrect body");
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(object.toJSONString()); }
         return null;
+    }
+
+    @GetMapping("mul")
+    public ResponseEntity<String> multiply(@RequestParam(value = "a") int value1, @RequestParam(value = "b") int value2){
+        System.out.println(value1 * value2);
+        JSONObject object = new JSONObject();
+        int result = value1 * value2;
+        object.put("result", result);
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(object.toJSONString());
     }
 
 }
--- src/main/java/sk/kosickaakademia/kristian/main/App.java
+++ src/main/java/sk/kosickaakademia/kristian/main/App.java
@@ -1,13 +1,15 @@
-package sk.kosickaakademia.kristian;
+package sk.kosickaakademia.kristian.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "sk.kosickaakademia.kristian.controller")
 public class App 
 {
     public static void main(String[] args) {
